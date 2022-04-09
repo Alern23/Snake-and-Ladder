f470e95864ce813f328186a7ff4440ddc7cc1832
Mindula Dilthushan Manamperi <minduladilthushan1@gmail.com>
2022-04-09 13:12:17 +0530

    game-init

---
+++ main.py
@@ -0,0 +1,12 @@
+# Snake and Ladder Game
+# mindula dilthushan
+# minduladilthushan1@gmail.com
+# 22-04-09
+
+def print_hi(game):
+
+    print(f'Welcome, {game}')
+
+if __name__ == '__main__':
+    print_hi('Snake and Ladder Game !')
+
