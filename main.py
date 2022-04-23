dbea24bc1a36a566d435b98d0773868efbc2298d
Mindula Dilthushan Manamperi <minduladilthushan1@gmail.com>
2022-04-23 09:45:24 +0530

    update-readme

--- main.py
+++ main.py
@@ -2,11 +2,20 @@
 # mindula dilthushan
 # minduladilthushan1@gmail.com
 # 22-04-09
+from random import random
+
+class Lottery:
+
+    @staticmethod
+    def getLottery():
+        return random.randint(1, 6)
+
 
 def print_hi(game):
-
     print(f'Welcome, {game}')
 
 if __name__ == '__main__':
     print_hi('Snake and Ladder Game !')
+
+
 
