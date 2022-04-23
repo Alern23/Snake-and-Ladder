55ea4b3a6c1796c74a831ec04005408257025af1
Mindula Dilthushan Manamperi <minduladilthushan1@gmail.com>
2022-04-23 10:35:25 +0530

    remove

--- demo.py
+++ demo.py
@@ -1,27 +1,65 @@
 import random
-import sys
+import time
+
+end = 100
+
+ladder = {1: 38, 4: 14, 8: 30, 21: 42, 28: 74, 50: 67, 71: 92, 88: 99}
+snake = {32: 10, 34: 6, 48: 26, 62: 18, 88: 89, 95: 56, 97: 78}
+
+def check_ladder(point):
+
+    if (point in ladder.keys()):
+        print("climb up!!")
+        return ladder[point]
+    else:
+        return point
+
+def check_snake(point):
+    if (point in snake.keys()):
+        print("ouch! snake bite!!")
+        return snake[point]
+    else:
+        return point
+
+def reached_end(point):
+    return (point >= end)
 
 
-def Move(value):
+def play():
 
+    p1_name = "mindula"
 
-    snake_squares = {16: 4, 22: 10, 33: 20, 48: 24, 62: 56, 78: 69, 74: 60, 91: 42, 97: 6}
-    ladder_squares = {3: 12, 7: 23, 11: 25, 21: 56, 47: 53, 60: 72, 80: 96}
-    Throw = random.randint(1, 6)
+    pp1 = 0
 
-    num = value + Throw
-    if num > 100:
-        print("BAD LUCK, YOU CANT MOVE, YOU NEED A {} TO WIN".format(100 - value))
-        return value
-    if num == 100:
-        return num
+    turn = 0
 
-    if num in snake_squares:
+    while (1):
+        if (turn % 2 == 0):
 
-        print(snake_squares[num])
-        num = snake_squares[num]
+            print(p1_name, "your turn")
 
-    elif num in ladder_squares:
-        num = ladder_squares[num]
+            if (1 == 0):
 
-    return num
+                print(p1_name, 'score : ', pp1)
+                print("Quiting the game!")
+                break
+
+            time.sleep(2)
+
+            dice = random.randint(1, 6)
+
+            print("Dice showed", dice)
+
+            pp1 += dice
+
+            pp1 = check_ladder(pp1)
+
+            pp1 = check_snake(pp1)
+
+            if pp1 > end:
+                pp1 = end
+            print(p1_name, 'Your score', pp1)
+            if (reached_end(pp1)):
+                print(p1_name, "won")
+
+play()
